598a585cc5c3ccd8efb2f661f38b10706394a7f5
abdel <yassir.montet70@gmail.com>
2022-11-23 19:41:45 +0100

    script import

--- import_local.sh
+++ import_local.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 curl -o import/pho_doc.csv  --silent http://cadic.cnd.fr:8000/exl-img/export/pho_doc_20221123.csv
-/usr/bin/php bin/console doctrine:fixtures:load --append
+php bin/console doctrine:fixtures:load --append
